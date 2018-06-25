37fd6e0d0c2b789f9b7c5b6b8451f3a4c1a8eb8d
Dustin Noyes <dnnoyes@amazon.com>
2018-06-25 15:59:20 -0700

    initial commit tutorial part 2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Amplify, { Analytics } from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 if (environment.production) {
   enableProdMode();
