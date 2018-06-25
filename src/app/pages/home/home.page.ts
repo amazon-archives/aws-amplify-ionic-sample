78be1fb930d22305feb9667440784a254dcb146a
Dustin Noyes <dnnoyes@amazon.com>
2018-06-25 16:23:10 -0700

    finalizing part 3

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, AfterContentInit } from '@angular/core';
 import { Events } from '@ionic/angular';
 import { AuthGuardService } from '../../services/auth-route-guard'
+import { AmplifyService }  from 'aws-amplify-angular';
 
 
 @Component({
@@ -13,24 +14,24 @@ export class HomePage implements AfterContentInit{
   authState: any;
   // including AuthGuardService here so that it's available to listen to auth events
   authService: AuthGuardService
+  amplifyService: AmplifyService
 
-  constructor(public events: Events, public guard: AuthGuardService) {
+  constructor(
+    public events: Events,
+    public guard: AuthGuardService,
+    public amplify: AmplifyService
+  ) {
     this.authState = {loggedIn: false};
     this.authService = guard;
+    this.amplifyService = amplify;
+    this.amplifyService.authStateChange$
+    .subscribe(authState => {
+      this.authState.loggedIn = authState.state === 'signedIn';
+      this.events.publish('data:AuthState', this.authState)
+    });
   }
 
   ngAfterContentInit(){
     this.events.publish('data:AuthState', this.authState)
   }
-
-  login() {
-    this.authState.loggedIn = true;
-    this.events.publish('data:AuthState', this.authState)
-  }
-
-  logout() {
-    this.authState.loggedIn = false;
-    this.events.publish('data:AuthState', this.authState)
-  }
-
 }
